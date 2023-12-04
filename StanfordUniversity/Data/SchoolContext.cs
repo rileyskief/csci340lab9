a45c3ccb919159e7d1092cea50ec07963b086b24
Riley Skief <skiefri@hendrix.edu>
2023-12-04 00:18:55 -0600

    tutorial complete

---
+++ StanfordUniversity/Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+#define First // LAST First
+#if First
+// <snippet_first>
+using StanfordUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace StanfordUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
+// </snippet_first>
+#elif LAST
+#endif
